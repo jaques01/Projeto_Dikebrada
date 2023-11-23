b72237bf344115a87a7ad01b3de57cb4372c2539
iamNicollas <nicollasguima12@gmail.com>
2023-11-23 17:24:12 -0300

    Update Modal Login - Autor: Nicollas | Data: 23/11

---
+++ Projeto Faculdade/js/modal_conta.js
@@ -0,0 +1,9 @@
+const dropdown = document.getElementById('dropdown');
+
+function showDropdown() {
+  dropdown.classList.add('active');
+}
+
+function hideDropdown() {
+  dropdown.classList.remove('active');
+}
